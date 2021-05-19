0c2e036102296ee7232b9888a55a9bd01be5d8fa
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-19 18:17:21 -0500

    Floating point precision

---
+++ src/academy/learnProgramming/FloatDouble.java
@@ -0,0 +1,31 @@
+package academy.learnProgramming;
+
+public class FloatDouble {
+    public static void main(String[] args) {
+         float myMinFloatValue = Float.MIN_VALUE;
+         float myMaxfloatValue = Float.MAX_VALUE;
+         System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxfloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5/2;
+        float myFloatValue = 5f/2f;
+        double myDoubleValue = 5d/2d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        int myIntValue1 = 5/3;
+        float myFloatValue1 = 5f/3f;
+        double myDoubleValue1 = 5d/3d;
+        System.out.println("MyIntValue1 = " + myIntValue1);
+        System.out.println("MyFloatValue1 = " + myFloatValue1);
+        System.out.println("MyDoubleValue1 = " + myDoubleValue1);
+
+
+    }
+}
