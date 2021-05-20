bb8f955ce44d9174a912ffdf57107f2a954d3309
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-20 12:51:36 -0500

    char and boolean data types

---
+++ src/Chars.java
@@ -0,0 +1,21 @@
+public class Chars {
+
+    public static void main(String[] args) {
+        char myChar ='D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar= '\u0049';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+        boolean isCustomerOverTwentyOne = true;
+        System.out.println(isCustomerOverTwentyOne);
+
+
+//        Char allows you to store unicode characters
+
+
+    }
+}
