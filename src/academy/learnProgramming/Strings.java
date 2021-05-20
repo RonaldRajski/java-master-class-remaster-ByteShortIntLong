879305be7cedfb95f5c155f0638f48a98061484f
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-20 13:35:01 -0500

    operators

---
+++ src/academy/learnProgramming/Strings.java
@@ -0,0 +1,32 @@
+package academy.learnProgramming;
+
+public class Strings {
+    public static void main(String[] args) {
+//        8 primitive dataTypes
+//        byte
+//        short
+//        int
+//        long
+//        float
+//        double
+//        char
+//        boolean
+        String myString = "This is a String";
+        System.out.println("MyString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("MyString is equal to " + myString);
+
+        int myInt= 50;
+        String lastString = null;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString =  lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
+
+
+
+
+
+    }
+}
