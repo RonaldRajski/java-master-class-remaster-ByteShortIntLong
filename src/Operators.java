5473d4a5906166077fb6ce3a7ae4ee92f2f8d2f5
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-21 12:21:44 -0500

    review and or

--- src/Operators.java
+++ src/Operators.java
@@ -42,6 +42,27 @@ public class Operators {
         System.out.println("10 - 2 = " + result);
 
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+
     }
 }
 
