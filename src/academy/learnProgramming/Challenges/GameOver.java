73b769673d421798b9aa6e999cfe84b38286f7b4
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-24 12:28:02 -0500

    lunch commit

--- src/academy/learnProgramming/Challenges/GameOver.java
+++ src/academy/learnProgramming/Challenges/GameOver.java
@@ -4,44 +4,20 @@ public class GameOver {
 
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-//        int score = 4000;
-//        int levelCompleted = 4;
-//        int bonus = 100;
-        int score = 10000;
-        int levelCompleted = 8;
-        int bonus = 200;
 
 
-//        if(score < 5000 && score > 1000){
-//            System.out.println("Your score was less than 5000 but but greater than 1000");
-//        }
-//        else if (score < 1000){
-//            System.out.println("Your score was less that 1000");
-//        }
-//        else {
-//            System.out.println("Got here");
-//        }
+        calculateScore(true, 800, 5, 100);
+        calculateScore(true, 1000, 8, 200);
 
+    }
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
 
         if (gameOver) {
-            int finalScore  = score + (levelCompleted * bonus);
-            finalScore +=1000;
+            int finalScore = score + (levelCompleted * bonus) + 2000;
             System.out.println("Your final score was " + finalScore);
         }
 
-//        int savedFinalScore = finalScore;
-
-//        boolean newGameOver =  true;
-//        int newScore = 10000;
-//        int newLevelCompleted = 8;
-//        int newBonus = 200;
-
-//        if (gameOver) {
-//            int finalScore  = newScore + (newLevelCompleted * newBonus);
-//            System.out.println("Your final score was " + finalScore);
-//        }
 
     }
 }
