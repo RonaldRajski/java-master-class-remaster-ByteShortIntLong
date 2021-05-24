5b2c8f6a355d0413b1541aff8beae386dc6a21ab
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-24 14:34:33 -0500

    else if challenge

--- src/academy/learnProgramming/Challenges/GameOver.java
+++ src/academy/learnProgramming/Challenges/GameOver.java
@@ -10,14 +10,74 @@ public class GameOver {
         int bonus = 100;
 
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
         score = 1000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(true, 1000, 8, 200);
+        highScore = calculateScore(true, 1000, 8, 200);
+        System.out.println("Your final score was " + highScore);
 
+
+    int highScorePosition = calculateHighScorePosition(1500);
+
+    displayHighScorePosition("Tim",highScorePosition);
+
+    highScorePosition =
+
+    calculateHighScorePosition(900);
+
+    displayHighScorePosition("Bob",highScorePosition);
+
+    highScorePosition =
+
+    calculateHighScorePosition(400);
+
+    displayHighScorePosition("Percy",highScorePosition);
+
+    highScorePosition =
+
+    calculateHighScorePosition(50);
+
+    displayHighScorePosition("Gilbert",highScorePosition);
+
+    calculateHighScorePosition(500);
+
+    displayHighScorePosition("Louise",highScorePosition);
+
+    calculateHighScorePosition(1000);
+
+    displayHighScorePosition("Frank",highScorePosition);
+
+    calculateHighScorePosition(100);
+
+    displayHighScorePosition("Carole",highScorePosition);
+
+    }
+
+
+
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition){
+        System.out.println(playerName + " managed to get into position " +
+               highScorePosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore >= 1000){
+            return 1;
+        }
+        else if (playerScore >=500 && playerScore <= 999){
+            return 2;
+        }
+        else if (playerScore >=100 && playerScore <= 499){
+            return 3;
+        }
+        else {
+            return 4;
+        }
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -25,7 +85,7 @@ public class GameOver {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus) + 2000;
-            System.out.println("Your final score was " + finalScore);
+//            System.out.println("Your final score was " + finalScore);
             return finalScore;
         } else {
             return -1;
