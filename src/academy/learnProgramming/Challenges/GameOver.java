c1fad129724e2f49905ce4a874c6c078b3be44c8
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-29 22:33:01 -0500

    removed redundant code

--- src/academy/learnProgramming/Challenges/GameOver.java
+++ src/academy/learnProgramming/Challenges/GameOver.java
@@ -66,10 +66,10 @@ public class GameOver {
         if(playerScore >= 1000){
             return 1;
         }
-        else if (playerScore >=500 && playerScore < 1000){
+        else if (playerScore >=500){
             return 2;
         }
-        else if (playerScore >=100 && playerScore < 500){
+        else if (playerScore >=100){
             return 3;
         }
         else {
