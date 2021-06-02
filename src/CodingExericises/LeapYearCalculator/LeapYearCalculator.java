293fc510ea55f4c6a436b5c52fe79b5951718500
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-02 18:47:53 -0500

    edits - running properly

--- src/CodingExericises/LeapYearCalculator/LeapYearCalculator.java
+++ src/CodingExericises/LeapYearCalculator/LeapYearCalculator.java
@@ -2,64 +2,33 @@ package CodingExericises.LeapYearCalculator;
 
 public class LeapYearCalculator {
 
-//    public static final int First_Leap_Year = 1752;
-//
-//    public boolean isLeapYear(int year){
-//        if(year<First_Leap_Year){
-//            throw new IllegalArgumentException("Cannot handle years before" + First_Leap_Year);
-//        }
-//        if(isDivisibleByOneHundred(year)){
-//            if(!isDivisbleByOneHundred(year)){
-//                return true;
-//            }
-//            else {
-//                return isDivisibleByFourHundre(year);
-//            }
-//            else{
-//                return false;
-//            }
-//        }
-//
-//        private boolean isDivisibleByFour(int year){
-//            return new Number(year).isDivisbleBy(4);
-//        }
-//        private boolean isDivisibleByOneHundred(int year){
-//            return new Number(year).isDivisbleBy(100);
-//        }
-//        private boolean isDivisibleByFourHundred(int year){
-//            return new Number(year).isDivisbleBy(400);
-//    }
-//
-//}
+    public static void main(String[] args) {
+        isLeapYear(-1600);
+        isLeapYear(1600);
+        isLeapYear(2017);
+        isLeapYear(2000);
+        isLeapYear(1924);
 
+    }
 
-    public static final int FIRST_LEAP_YEAR = 1752;
-
-    public boolean isLeapYear(int year) {
-        if (year<FIRST_LEAP_YEAR) {
-            throw new IllegalArgumentException("Cannot handle years before " + FIRST_LEAP_YEAR);
-        }
-        if (isDivisibleByFour(year)) {
-            //noinspection SimplifiableIfStatement
-            if (!isDivisibleByOneHundred(year)) {
-                return true;
-            }  else {
-                return isDivisibleByFourHundred(year);
-            }
-        } else {
+    public static boolean isLeapYear(int year){
+        if(year < 1 || year > 9999) {
+            System.out.println("false");
             return false;
+        } else {
+            if((year % 4 == 0) && (year % 100 > 0)){
+                System.out.println("true");
+                return true;
+            } else if (year % 400 == 0){
+                System.out.println("true");
+                return true;
+            } else {
+                System.out.println("false");
+                return false;
+            }
         }
-    }
-
-    private boolean isDivisibleByFour(int year) {
-        return new Number(year).isDivisibleBy(4);
-    }
-
-    private boolean isDivisibleByOneHundred(int year) {
-        return new Number(year).isDivisibleBy(100);
-    }
-
-    private boolean isDivisibleByFourHundred(int year) {
-        return new Number(year).isDivisibleBy(400);
     }
 }
+
+
+
