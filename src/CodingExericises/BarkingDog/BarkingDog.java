b651b89fa6cade6e818c9b64b450cea74b851c0f
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-02 18:14:46 -0500

    barkingDog

---
+++ src/CodingExericises/BarkingDog/BarkingDog.java
@@ -0,0 +1,16 @@
+package CodingExericises.BarkingDog;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23 || !barking) {
+            return false;
+        } else if (barking && hourOfDay < 8) {
+            return true;
+        } else if (barking && hourOfDay > 22) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
