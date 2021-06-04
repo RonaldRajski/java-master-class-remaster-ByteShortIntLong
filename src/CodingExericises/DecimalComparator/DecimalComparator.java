3a98eb46eed1f73608e654e354e78e1d7e937007
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-03 23:02:42 -0500

    need to complete main

--- src/CodingExericises/DecimalComparator/DecimalComparator.java
+++ src/CodingExericises/DecimalComparator/DecimalComparator.java
@@ -1,4 +1,11 @@
 package CodingExericises.DecimalComparator;
 
 public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber){
+
+        if(((int) (firstNumber * 1000)) == ((int) (secondNumber * 1000))){
+            return true;
+        }
+        return false;
+    }
 }
--- src/CodingExericises/DecimalComparator/Main.java
+++ src/CodingExericises/DecimalComparator/Main.java
@@ -1,4 +1,5 @@
 package CodingExericises.DecimalComparator;
 
 public class Main {
+
 }
