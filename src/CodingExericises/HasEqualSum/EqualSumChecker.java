1c3ba173533cd3e23434c69cca7cc78c685de3a9
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-03 23:16:14 -0500

    completed challenge

---
+++ src/CodingExericises/HasEqualSum/EqualSumChecker.java
@@ -0,0 +1,10 @@
+package CodingExericises.HasEqualSum;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum (int firstNumber, int secondNumber, int thirdNumber){
+        if ((firstNumber + secondNumber) == thirdNumber){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/CodingExericises/HasEqualSum/Main.java
@@ -0,0 +1,13 @@
+package CodingExericises.HasEqualSum;
+
+import CodingExericises.DecimalComparator.DecimalComparator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(EqualSumChecker.hasEqualSum(1,1,1)); //false
+        System.out.println(EqualSumChecker.hasEqualSum(1,1,2)); //true
+        System.out.println(EqualSumChecker.hasEqualSum(1,-1,0)); //true
+
+
+    }
+}
