0cff6b310d5652c2fc44acd84719bc499ae037dd
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-05 11:47:51 -0500

    showing error

--- src/CodingExericises/HasTeenChecker/Main.java
+++ src/CodingExericises/HasTeenChecker/Main.java
@@ -2,6 +2,7 @@ package CodingExericises.HasTeenChecker;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(hasTeen (12,44, 19));
+        System.out.println(isTeen(13));
     }
 }
