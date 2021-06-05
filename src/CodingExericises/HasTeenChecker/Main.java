04266725d843bf9e062a3852d3a22f428c268fc5
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-05 11:50:19 -0500

    working had to import static method

--- src/CodingExericises/HasTeenChecker/Main.java
+++ src/CodingExericises/HasTeenChecker/Main.java
@@ -1,5 +1,8 @@
 package CodingExericises.HasTeenChecker;
 
+import static CodingExericises.HasTeenChecker.TeenNumberChecker.hasTeen;
+import static CodingExericises.HasTeenChecker.TeenNumberChecker.isTeen;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(hasTeen (12,44, 19));
--- src/CodingExericises/HasTeenChecker/TeenNumberChecker.java
+++ src/CodingExericises/HasTeenChecker/TeenNumberChecker.java
@@ -1,18 +1,19 @@
 package CodingExericises.HasTeenChecker;
 
 public class TeenNumberChecker {
-    public static boolean hasTeen(int i, int i1, int i2){
-        if((i>=13 && i<=19) || (i1>=13 && i1<=19)|| (i2>=13 && i2<=19)){
+    public static boolean hasTeen(int i, int i1, int i2) {
+        if ((i >= 13 && i <= 19) || (i1 >= 13 && i1 <= 19) || ((i2 >= 13) && (i2 <= 19))) {
             return true;
-        }
-        return false;
-    }
-    public static boolean isTeen(int a){
-        if(a>=13 || a<=19){
-            return true;
-        }
-        return false;
+        } else
+            return false;
     }
 
+    public static boolean isTeen(int b) {
+        if (b >= 13 && b <= 19)
+            return true;
+        else
+            return false;
 
+
+    }
 }
