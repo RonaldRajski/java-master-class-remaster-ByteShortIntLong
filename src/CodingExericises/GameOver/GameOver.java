20a5841aed4145d8dd04e5c042d29701072ce8cb
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-08 23:26:01 -0500

    code cleanup

--- src/CodingExericises/GameOver/GameOver.java
+++ src/CodingExericises/GameOver/GameOver.java
@@ -1,4 +1,4 @@
-package academy.learnProgramming.Challenges;
+package CodingExericises.GameOver;
 
 public class GameOver {
 
--- src/CodingExericises/LbToKilo/LbToKilo.java
+++ src/CodingExericises/LbToKilo/LbToKilo.java
@@ -1,4 +1,4 @@
-package academy.learnProgramming.Challenges;
+package CodingExericises.LbToKilo;
 
 public class LbToKilo {
 
