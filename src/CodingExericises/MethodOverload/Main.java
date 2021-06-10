40d71d82cb5a98144013a658026e82494dbe9641
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-10 17:54:27 -0500

    almost complete

---
+++ src/CodingExericises/MethodOverload/Main.java
@@ -0,0 +1,26 @@
+package CodingExericises.MethodOverload;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Ron", 500);
+        System.out.println("New Score is: " + newScore);
+        calculateScore(75);
+        calculateScore();
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player socred " + score + " points");
+        return score * 1000;
+    }
+    public static int calculateScore(){
+        System.out.println("No Player name, no player score");
+        return 0;
+    }
+
+
+}
