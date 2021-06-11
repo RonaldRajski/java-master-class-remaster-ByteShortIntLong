f6e012be511c899fab8e922c9b8e47b75b22495c
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-11 12:11:53 -0500

    added bonus, program working as expected

--- src/CodingExericises/MethodOverload/SecondsAndMinutes/Main.java
+++ src/CodingExericises/MethodOverload/SecondsAndMinutes/Main.java
@@ -18,7 +18,18 @@ public class Main {
         if(hours <10){
             hoursString = "0" + hoursString;
         }
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String minutesString = remainingMinutes + "m";
+        if(remainingMinutes <10){
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if(seconds <10){
+            secondsString = "0" + secondsString;
+        }
+        return hoursString + " " +  minutesString + " " + secondsString + ".";
+
     }
 
     private static String getDurationString(long seconds) {
