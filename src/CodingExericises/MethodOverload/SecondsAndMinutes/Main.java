0742ea00bbc68e5c7a06d5d378958df7b7d33a2b
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-11 12:02:02 -0500

    bonus init

--- src/CodingExericises/MethodOverload/SecondsAndMinutes/Main.java
+++ src/CodingExericises/MethodOverload/SecondsAndMinutes/Main.java
@@ -3,7 +3,8 @@ package CodingExericises.MethodOverload.SecondsAndMinutes;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(65L,45L));
+        System.out.println(getDurationString(3945L));
 
     }
     private static String getDurationString(long minutes, long seconds){
@@ -12,8 +13,25 @@ public class Main {
         }
         long hours = minutes/60;
         long remainingMinutes = minutes % 60;
+
+        String hoursString = hours + "h";
+        if(hours <10){
+            hoursString = "0" + hoursString;
+        }
         return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
+
+    private static String getDurationString(long seconds) {
+        if (seconds < 0) {
+            System.out.println("Invalid value");
+        }
+
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
+    }
+
 
 
 
