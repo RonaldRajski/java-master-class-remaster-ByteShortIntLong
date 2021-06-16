9f56cf696779b425d6bb62e0bf75470b50ff46e7
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-16 17:22:48 -0500

    switch statement

---
+++ src/ControlFlowStatements/Switch/Switch.java
@@ -0,0 +1,60 @@
+package ControlFlowStatements.Switch;
+
+import com.sun.jdi.Value;
+
+public class Switch {
+    public static void main(String[] args) {
+//        int value = 1;
+//        if (value ==1){
+//            System.out.println("Value was 1");
+//        }
+//        else if(value == 2){
+//            System.out.println("Value was 2");
+//        }
+//        else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("Was a 3,4, or 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+
+        char grade = 'F';
+        switch (grade){
+            case 'A':
+                System.out.println("Grade was an A");
+                break;
+
+            case 'B':
+                System.out.println("Grade was a B");
+                break;
+
+            case 'C': case 'D':
+                System.out.println("Was a C or D");
+                System.out.println("Actually it was a " + grade);
+                break;
+
+            default:
+                System.out.println("Like the Playoff chances of the 2021 Chicago BlackHawks, An F is a failing grade");
+                break;
+        }
+
+
+    }
+}
