886bc724f36441806a2fcfa890a1743a948b16eb
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-19 12:14:06 -0500

    for loop review

---
+++ src/ControlFlowStatements/Switch/ForStatement/Main.java
@@ -0,0 +1,11 @@
+package ControlFlowStatements.Switch.ForStatement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 5.0));
+    }
+}
