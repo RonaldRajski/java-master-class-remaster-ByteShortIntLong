3b77f5ca3d3ba51ecf164375bf11491b2b66f4f0
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-21 20:20:47 -0500

    for loop 7% interest showing up weird

--- src/ControlFlowStatements/Switch/ForStatement/Main.java
+++ src/ControlFlowStatements/Switch/ForStatement/Main.java
@@ -3,9 +3,28 @@ package ControlFlowStatements.Switch.ForStatement;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 2.0));
-        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 3.0));
-        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 4.0));
-        System.out.println("10,000 at 2% interest =" + calculateInterest(10000.0, 5.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 5.0));
+
+//        for(init; termination(exit); increment)
+
+        for(int i = 0; i < 5 ;i++){
+            System.out.println("loop " + i +  " Hello!");
+        }
+
+        for(int i = 2; i < 9; i++){
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, (double) i));
+        }
+
+
+    }
+
+
+
+    public static double calculateInterest(double amount, double interestRate){
+
+        return (amount*(interestRate/100));
     }
 }
