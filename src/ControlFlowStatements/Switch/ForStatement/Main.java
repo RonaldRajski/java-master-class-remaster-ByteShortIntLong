bb07fa321d49a73d6d8821ca35ebe55966980791
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-22 22:52:58 -0500

    finished is prime

--- src/ControlFlowStatements/Switch/ForStatement/Main.java
+++ src/ControlFlowStatements/Switch/ForStatement/Main.java
@@ -10,32 +10,53 @@ public class Main {
 
 //        for(init; termination(exit); increment)
 
-        for(int i = 0; i < 5 ;i++){
-            System.out.println("loop " + i +  " Hello!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("loop " + i + " Hello!");
         }
 
-        for(int i = 2; i < 9; i++){
-            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0,i)));
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
-        for(int i = 8; i > 1; i--){
-            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0,i)));
+        for (int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
+        int count = 0;
+        for (int i = 10; 1 < 50; i++) {
+            if(isPrime(i)) {
+                count ++;
+                System.out.println("Number " + i + " is a prime number");
+                if(count==3){
+                    System.out.println("Exiting for loop");
+                    break;
+            }
 
+
+        }
     }
+}
 
-    public static boolean isPrime(int n){
-        return false;
-    }
 
-    for(int i=2; i <= n/2; i++){
-        if(n%i == 0){
+
+
+//    Challenge
+
+    public static boolean isPrime(int n) {
+
+        if (n == 1) {
             return false;
         }
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    return true;
+
 
 
 
