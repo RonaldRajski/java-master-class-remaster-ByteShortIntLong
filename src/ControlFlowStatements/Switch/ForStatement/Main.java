71837eb6c3a931e29da873c9c0241eee06b0c153
Ronald Rajski <ronaldrajski@gmail.com>
2021-06-22 22:32:19 -0500

    added initial info for prime challenge

--- src/ControlFlowStatements/Switch/ForStatement/Main.java
+++ src/ControlFlowStatements/Switch/ForStatement/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 3.0));
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 4.0));
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 5.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
 
 //        for(init; termination(exit); increment)
 
@@ -18,8 +18,24 @@ public class Main {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0,i)));
         }
 
+        for(int i = 8; i > 1; i--){
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0,i)));
+        }
+
 
     }
+
+    public static boolean isPrime(int n){
+        return false;
+    }
+
+    for(int i=2; i <= n/2; i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+
+    return true;
 
 
 
