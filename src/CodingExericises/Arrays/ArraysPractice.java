19a8ae991e2b8c62b42d13fe7ef345f3ef6d67fc
Ronald Rajski <ronaldrajski@gmail.com>
2022-02-22 15:06:42 -0600

    Array Practice

---
+++ src/CodingExericises/Arrays/ArraysPractice.java
@@ -0,0 +1,41 @@
+package CodingExericises.Arrays;
+
+public class ArraysPractice {
+//    public static void main(String[] args) {
+        public static int countEven(int[] arr){
+            int count = 0;
+            for (int num : arr) {
+                if (num % 2 == 0) {
+                    count++;
+
+                    return count;
+                }
+            }
+
+        }
+    }
+//}
+//        int[] myIntArray = new int[25];
+//
+//        for (int i = 0; i < myIntArray.length; i++) {
+//            myIntArray[i] = i * 10;
+//
+//        }
+////        for (int i = 0; i < myIntArray.length; i++){
+////            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+//
+//        printArray(myIntArray);
+//    }
+
+
+    //    Create an array using a method
+//    public static void printArray(int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println("Element " + i + ", value is " + array[i]);
+//
+//        }
+//    }
+//    Return the nuber of even integers in array arr of integers
+
+
+
