ffb79300a0a0170c56320d0d9ba5a87cf6c25960
Ronald Rajski <ronaldrajski@gmail.com>
2022-02-23 14:37:05 -0600

    Arrays day 2

--- src/CodingExericises/Arrays/ArraysPractice.java
+++ src/CodingExericises/Arrays/ArraysPractice.java
@@ -1,19 +1,49 @@
 package CodingExericises.Arrays;
+import java.util.Scanner;
 
 public class ArraysPractice {
-//    public static void main(String[] args) {
-        public static int countEven(int[] arr){
-            int count = 0;
-            for (int num : arr) {
-                if (num % 2 == 0) {
-                    count++;
 
-                    return count;
-                }
-            }
+    private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+
+
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
+        for (int i = 0; i < array.length; i++) {
+            array[i] =scanner.nextInt();
+        }
+        return array;
+    }
+    public static void printArray(int[]array){
+        for(int i = 0; i< array.length; i++){
+            System.out.println("Element " + i + "contents " + array[i]);
         }
     }
+
+    public static in[] sortInteger(int[array]){
+        int sortedArray = new int[arry.length];
+
+
+    }
+
+}
+//        public static int countEven(int[] arr){
+//            int count = 0;
+//            for (int num : arr) {
+//                if (num % 2 == 0) {
+//                    count++;
+//
+//                    return count;
+//                }
+//            }
+//
+//        }
+//    }
 //}
 //        int[] myIntArray = new int[25];
 //
@@ -35,7 +65,7 @@ public class ArraysPractice {
 //
 //        }
 //    }
-//    Return the nuber of even integers in array arr of integers
+//    Return the number of even integers in array arr of integers
 
 
 
